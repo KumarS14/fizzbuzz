0bd08414e689aaa7bd4cf32839482bdb9e90a1b2
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-08 14:59:23 +0000

    test for number

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -30,4 +30,13 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed multiple of 5' do
     expect(fizzbuzz(50)).to eq 'buzz'
   end
+  it 'returns "number" when passed a number' do
+    expect(fizzbuzz(2)).to eq '2'
+  end
+  it 'returns "number" when passed a number' do
+    expect(fizzbuzz(4)).to eq '4'
+  end
+  it 'returns "number" when passed a number' do
+    expect(fizzbuzz(7)).to eq '7'
+  end
 end
