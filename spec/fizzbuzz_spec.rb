cfac55917eee3257d67cc7db182a440c267f9bcf
Valeria Serci <balerty84@gmail.com>
2023-02-08 14:45:57 +0000

    test for multiple of 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,14 @@
 require './lib/fizzbuzz'
 describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
+  end
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end 
