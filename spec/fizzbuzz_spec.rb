96669c1d29a11159f237eb5fc918dd55192741dd
Valeria Serci <balerty84@gmail.com>
2023-02-08 12:39:10 +0000

    add test for multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,13 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end 
+  it 'returns "fizz" when passed multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+  it 'returns "fizz" when passed multiple of 3' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  it 'returns "fizz" when passed multiple of 3' do
+    expect(fizzbuzz(12)).to eq 'fizz'
+  end
 end
